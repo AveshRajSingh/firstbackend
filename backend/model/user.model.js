5db6b56e0c1b5b1f1ea0d948d4bcfd677c787854
Avesh Raj Singh <aveshrajsingh3@gmail.com>
2025-01-30 21:15:34 +0530

    add user schema

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,5 +1,41 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({},{timestamps:true})
+const userSchema = mongoose.Schema({
+    name :{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true
+    },
+    email:{
+        type:String,
+        unique:true,
+        index:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    cart:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref: "Cart"
+        }
+    ],
+    isAdmin :Boolean,
+    contact:{
+        type: Number,
+        
+    },
+    picture:{
+        type:String
+    }
+
+
+},{timestamps:true})
 
 export const User = mongoose.model("User",userSchema)
